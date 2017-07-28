46c90be0017878949bdb9f22ec9fd5d10a8428eb
stephane <stephane.wong1@gmail.com>
2017-07-28 15:42:03 +0200

    Constant database fields added

---
+++ src/main/scala/models/db/Database.scala
@@ -0,0 +1,28 @@
+package models.db
+
+/**
+  * Created by stephane on 28/07/2017.
+  */
+object DatabaseNames {
+
+  final val ODLychee = "ODLychee"
+  final val OGLychee = "OGLychee"
+
+
+}
+
+object DatabaseCollections {
+
+  final val Users = "users"
+  final val Tasks = "tasks"
+
+}
+
+
+object DatabaseFields {
+
+  final val Name = "name"
+  final val Email = "email"
+  final val password = "password"
+  
+}
